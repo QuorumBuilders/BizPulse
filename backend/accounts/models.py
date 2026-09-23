4855808b29de77701e0f9de3cf93aa6fed2f7dc0
Granzular <ayenimichael92@gmail.com>
2026-09-23 02:25:49 +0100

    (feat) added  Business and Customer models under accounts app

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Business(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="businesses",
+    )
+    name = models.CharField(max_length=150)
+    starting_cash = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
+    )
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    purge_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Customer(models.Model):
+    business = models.ForeignKey(
+        "accounts.Business",
+        on_delete=models.CASCADE,
+        related_name="customers",
+    )
+    name = models.CharField(max_length=150)
+    phone = models.CharField(
+        max_length=30,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
